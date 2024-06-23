c27a78f8e6affe459b2d65f961387b3cb8cfb242
Donnie Pitts <contact@donniepitts.com>
2024-06-23 16:33:30 -0500

    Update camera effects

--- Scenes/Camera.gd
+++ Scenes/Camera.gd
@@ -7,7 +7,7 @@ var _zoom: Vector2
 
 var interval: float
 
-const BARS_NEEDED_BEFORE_EFFECTS: int = 0 #9 * 4
+const BARS_NEEDED_BEFORE_EFFECTS: int = 4 * 4
 const BEATS_TO_ZOOM: int = 4
 var beat_count: int
 
@@ -19,20 +19,22 @@ func _ready():
 	interval = 60 / DifficultyManager.bpm
 
 func _on_conductor_quarter_will_pass(beat):
-	beat_count += 1
-	if beat > BARS_NEEDED_BEFORE_EFFECTS and beat_count > BEATS_TO_ZOOM:
-		beat_count = 0
-		
-		# Tween Zoom
-		var tween = create_tween()
-		tween.tween_property(self, "zoom", Vector2(1.675, 1.675), interval / 3)
-		#tween.set_trans(Tween.TRANS_ELASTIC)
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.chain().tween_property(self, "zoom", _zoom, interval / 3)
+	if beat > BARS_NEEDED_BEFORE_EFFECTS:
+		if not int(beat)%BEATS_TO_ZOOM:
+			beat_count += 1
+			beat_count = 0
+			
+			# Tween Zoom
+			var tween = create_tween()
+			var zoom_amt = randf_range(1.25, 1.675)
+			tween.tween_property(self, "zoom", Vector2(zoom_amt, zoom_amt), interval / 3)
+			#tween.set_trans(Tween.TRANS_ELASTIC)
+			tween.set_ease(Tween.EASE_IN_OUT)
+			tween.chain().tween_property(self, "zoom", _zoom, interval / 3)
 
-		# Tween Position
-		var pos_tween = create_tween()
-		pos_tween.tween_property(self, "position", Vector2(eye.global_position.x, eye.global_position.y / 2), interval / 3)
-		tween.set_trans(Tween.TRANS_ELASTIC)
-		tween.set_ease(Tween.EASE_IN_OUT)
-		pos_tween.tween_property(self, "position", _position, interval / 2)
+			# Tween Position
+			var pos_tween = create_tween()
+			pos_tween.tween_property(self, "position", Vector2(eye.global_position.x, eye.global_position.y / 2), interval / 3)
+			tween.set_trans(Tween.TRANS_ELASTIC)
+			tween.set_ease(Tween.EASE_IN_OUT)
+			pos_tween.tween_property(self, "position", _position, interval / 2)
