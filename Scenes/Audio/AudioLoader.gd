e3c9d1e217275cac6b2b020dac9a395642dbdc96
George Meisinger <gmeisinger@Gmail.com>
2024-06-23 14:09:42 -0500

    - fixed web audio

--- Scenes/Audio/AudioLoader.gd
+++ Scenes/Audio/AudioLoader.gd
@@ -49,109 +49,110 @@ func report_errors(err, filepath):
 		print("Unknown error with file ", filepath, " error code: ", err)
 
 func loadfile(filepath) -> AudioStreamWAV:
-	var file = FileAccess.open(filepath, FileAccess.READ)
-	var bytes = file.get_buffer(file.get_length())
-	
-	var newstream = AudioStreamWAV.new()
-	
-	if filepath.ends_with(".wav"):
-
-		#---------------------------
-		#parrrrseeeeee!!! :D
-		
-		var bits_per_sample = 0
-		var i = 0
-		while true:
-			if i >= len(bytes) - 4: # Failsafe, if there is no data bytes
-				print("Data byte not found")
-				break
-				
-			var those4bytes = str(char(bytes[i])+char(bytes[i+1])+char(bytes[i+2])+char(bytes[i+3]))
-			
-			if those4bytes == "RIFF": 
-				print ("RIFF OK at bytes " + str(i) + "-" + str(i+3))
-				#RIP bytes 4-7 integer for now
-			if those4bytes == "WAVE": 
-				print ("WAVE OK at bytes " + str(i) + "-" + str(i+3))
-
-			if those4bytes == "fmt ":
-				print ("fmt OK at bytes " + str(i) + "-" + str(i+3))
-				
-				#get format subchunk size, 4 bytes next to "fmt " are an int32
-				var formatsubchunksize = bytes[i+4] + (bytes[i+5] << 8) + (bytes[i+6] << 16) + (bytes[i+7] << 24)
-				print ("Format subchunk size: " + str(formatsubchunksize))
-				
-				#using formatsubchunk index so it's easier to understand what's going on
-				var fsc0 = i+8 #fsc0 is byte 8 after start of "fmt "
-
-				#get format code [Bytes 0-1]
-				var format_code = bytes[fsc0] + (bytes[fsc0+1] << 8)
-				var format_name
-				if format_code == 0: format_name = "8_BITS"
-				elif format_code == 1: format_name = "16_BITS"
-				elif format_code == 2: format_name = "IMA_ADPCM"
-				else: 
-					format_name = "UNKNOWN (trying to interpret as 16_BITS)"
-					format_code = 1
-				print ("Format: " + str(format_code) + " " + format_name)
-				#assign format to our AudioStreamSample
-				newstream.format = format_code
-				
-				#get channel num [Bytes 2-3]
-				var channel_num = bytes[fsc0+2] + (bytes[fsc0+3] << 8)
-				print ("Number of channels: " + str(channel_num))
-				#set our AudioStreamSample to stereo if needed
-				if channel_num == 2: newstream.stereo = true
-				
-				#get sample rate [Bytes 4-7]
-				var sample_rate = bytes[fsc0+4] + (bytes[fsc0+5] << 8) + (bytes[fsc0+6] << 16) + (bytes[fsc0+7] << 24)
-				print ("Sample rate: " + str(sample_rate))
-				#set our AudioStreamSample mixrate
-				newstream.mix_rate = sample_rate
-				
-				#get byte_rate [Bytes 8-11] because we can
-				var byte_rate = bytes[fsc0+8] + (bytes[fsc0+9] << 8) + (bytes[fsc0+10] << 16) + (bytes[fsc0+11] << 24)
-				print ("Byte rate: " + str(byte_rate))
-				
-				#same with bits*sample*channel [Bytes 12-13]
-				var bits_sample_channel = bytes[fsc0+12] + (bytes[fsc0+13] << 8)
-				print ("BitsPerSample * Channel / 8: " + str(bits_sample_channel))
-				
-				#aaaand bits per sample/bitrate [Bytes 14-15]
-				bits_per_sample = bytes[fsc0+14] + (bytes[fsc0+15] << 8)
-				print ("Bits per sample: " + str(bits_per_sample))
-				
-			if those4bytes == "data":
-				assert(bits_per_sample != 0)
-				
-				var audio_data_size = bytes[i+4] + (bytes[i+5] << 8) + (bytes[i+6] << 16) + (bytes[i+7] << 24)
-				print ("Audio data/stream size is " + str(audio_data_size) + " bytes")
-
-				var data_entry_point = (i+8)
-				print ("Audio data starts at byte " + str(data_entry_point))
-				
-				var data = bytes.slice(data_entry_point, data_entry_point+audio_data_size-1)
-				
-				if bits_per_sample in [24, 32]:
-					newstream.data = convert_to_16bit(data, bits_per_sample)
-				else:
-					newstream.data = data
-				
-				break # the data will be at the end, end searching here
-			
-			i += 1
-			# end of parsing
-			#---------------------------
-
-		#get samples and set loop end
-		var samplenum = newstream.data.size() / 4
-		newstream.loop_end = samplenum
-		newstream.loop_mode = 1 #change to 0 or delete this line if you don't want loop, also check out modes 2 and 3 in the docs
-
-	else:
-		print ("ERROR: Wrong filetype or format")
-	
-	return newstream
+	return ResourceLoader.load(filepath)
+	#var file = FileAccess.open(filepath, FileAccess.READ)
+	#var bytes = file.get_buffer(file.get_length())
+	#
+	#var newstream = AudioStreamWAV.new()
+	#
+	#if filepath.ends_with(".wav"):
+#
+		##---------------------------
+		##parrrrseeeeee!!! :D
+		#
+		#var bits_per_sample = 0
+		#var i = 0
+		#while true:
+			#if i >= len(bytes) - 4: # Failsafe, if there is no data bytes
+				#print("Data byte not found")
+				#break
+				#
+			#var those4bytes = str(char(bytes[i])+char(bytes[i+1])+char(bytes[i+2])+char(bytes[i+3]))
+			#
+			#if those4bytes == "RIFF": 
+				#print ("RIFF OK at bytes " + str(i) + "-" + str(i+3))
+				##RIP bytes 4-7 integer for now
+			#if those4bytes == "WAVE": 
+				#print ("WAVE OK at bytes " + str(i) + "-" + str(i+3))
+#
+			#if those4bytes == "fmt ":
+				#print ("fmt OK at bytes " + str(i) + "-" + str(i+3))
+				#
+				##get format subchunk size, 4 bytes next to "fmt " are an int32
+				#var formatsubchunksize = bytes[i+4] + (bytes[i+5] << 8) + (bytes[i+6] << 16) + (bytes[i+7] << 24)
+				#print ("Format subchunk size: " + str(formatsubchunksize))
+				#
+				##using formatsubchunk index so it's easier to understand what's going on
+				#var fsc0 = i+8 #fsc0 is byte 8 after start of "fmt "
+#
+				##get format code [Bytes 0-1]
+				#var format_code = bytes[fsc0] + (bytes[fsc0+1] << 8)
+				#var format_name
+				#if format_code == 0: format_name = "8_BITS"
+				#elif format_code == 1: format_name = "16_BITS"
+				#elif format_code == 2: format_name = "IMA_ADPCM"
+				#else: 
+					#format_name = "UNKNOWN (trying to interpret as 16_BITS)"
+					#format_code = 1
+				#print ("Format: " + str(format_code) + " " + format_name)
+				##assign format to our AudioStreamSample
+				#newstream.format = format_code
+				#
+				##get channel num [Bytes 2-3]
+				#var channel_num = bytes[fsc0+2] + (bytes[fsc0+3] << 8)
+				#print ("Number of channels: " + str(channel_num))
+				##set our AudioStreamSample to stereo if needed
+				#if channel_num == 2: newstream.stereo = true
+				#
+				##get sample rate [Bytes 4-7]
+				#var sample_rate = bytes[fsc0+4] + (bytes[fsc0+5] << 8) + (bytes[fsc0+6] << 16) + (bytes[fsc0+7] << 24)
+				#print ("Sample rate: " + str(sample_rate))
+				##set our AudioStreamSample mixrate
+				#newstream.mix_rate = sample_rate
+				#
+				##get byte_rate [Bytes 8-11] because we can
+				#var byte_rate = bytes[fsc0+8] + (bytes[fsc0+9] << 8) + (bytes[fsc0+10] << 16) + (bytes[fsc0+11] << 24)
+				#print ("Byte rate: " + str(byte_rate))
+				#
+				##same with bits*sample*channel [Bytes 12-13]
+				#var bits_sample_channel = bytes[fsc0+12] + (bytes[fsc0+13] << 8)
+				#print ("BitsPerSample * Channel / 8: " + str(bits_sample_channel))
+				#
+				##aaaand bits per sample/bitrate [Bytes 14-15]
+				#bits_per_sample = bytes[fsc0+14] + (bytes[fsc0+15] << 8)
+				#print ("Bits per sample: " + str(bits_per_sample))
+				#
+			#if those4bytes == "data":
+				#assert(bits_per_sample != 0)
+				#
+				#var audio_data_size = bytes[i+4] + (bytes[i+5] << 8) + (bytes[i+6] << 16) + (bytes[i+7] << 24)
+				#print ("Audio data/stream size is " + str(audio_data_size) + " bytes")
+#
+				#var data_entry_point = (i+8)
+				#print ("Audio data starts at byte " + str(data_entry_point))
+				#
+				#var data = bytes.slice(data_entry_point, data_entry_point+audio_data_size-1)
+				#
+				#if bits_per_sample in [24, 32]:
+					#newstream.data = convert_to_16bit(data, bits_per_sample)
+				#else:
+					#newstream.data = data
+				#
+				#break # the data will be at the end, end searching here
+			#
+			#i += 1
+			## end of parsing
+			##---------------------------
+#
+		##get samples and set loop end
+		#var samplenum = newstream.data.size() / 4
+		#newstream.loop_end = samplenum
+		#newstream.loop_mode = 1 #change to 0 or delete this line if you don't want loop, also check out modes 2 and 3 in the docs
+#
+	#else:
+		#print ("ERROR: Wrong filetype or format")
+	#
+	#return newstream
 
 # Converts .wav data from 24 or 32 bits to 16
 #
