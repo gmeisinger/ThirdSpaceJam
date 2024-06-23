07da3f1ec341dbc19ca5e0ae54debb3518a753b6
George Meisinger <gmeisinger@Gmail.com>
2024-06-22 21:01:32 -0500

    - gameover loop working

---
+++ Scenes/Screens/gameover.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@export var canvas_modulate : CanvasModulate
+@export var next_scene : String
+
+var tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tween = get_tree().create_tween()
+	tween.tween_property(canvas_modulate, "color", Color(1,1,1), 1.0)
+
+func _on_tween_finished():
+	get_tree().change_scene_to_file(next_scene)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("OK"):
+		tween.stop()
+		tween = get_tree().create_tween()
+		tween.connect("finished", _on_tween_finished)
+		tween.tween_property(canvas_modulate, "color", Color(0,0,0), 1.0)
